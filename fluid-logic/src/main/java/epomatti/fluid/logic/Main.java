e3ecbbb32378f6627b3f4b5e698dc8896c6dfad6
Evandro Pomatti <TECBMEPI@TECLOGICA.COM.BR>
2019-05-23 10:50:39 -0300

    tests with GraalVM

--- fluid-logic/src/main/java/epomatti/fluid/logic/Main.java
+++ fluid-logic/src/main/java/epomatti/fluid/logic/Main.java
@@ -7,37 +7,28 @@ import java.io.Reader;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
-        String path = "rules.js";
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
+		String path = "rules.js";
 
-        try ( InputStream is = Main.class.getClassLoader().getResourceAsStream(path);  Reader reader = new InputStreamReader(is)) {
+		try (InputStream is = Main.class.getClassLoader().getResourceAsStream(path);
+				Reader reader = new InputStreamReader(is)) {
 
-            engine.eval(reader);
-            Invocable inv = (Invocable) engine;
-            Object obj = engine.get("obj");
+			engine.eval(reader);
+			Invocable inv = (Invocable) engine;
+			Object obj = engine.get("obj");
 
-            // Validação e retorno de valores
-            User user = new User();
-            user.setName("Evandro");
-            user.setProfile("admin");
+			User user = new User();
+			user.setName("username");
+			user.setProfile("admin");
 
-            Boolean retorno = (Boolean) inv.invokeMethod(obj, "validatePermission", user);
-            System.out.println("Validação usuário: " + retorno);
+			Boolean retorno = (Boolean) inv.invokeMethod(obj, "validatePermission", user);
+			System.out.println("Validation: " + retorno);
 
-            // Executes and catches the exception thrown by the JavaScript
-            try {
-                user.setProfile("user");
-                inv.invokeMethod(obj, "validatePermission", user);
-            } catch (ScriptException e) {
-                System.err.println("Error returned from script validation: " + e.getMessage());
-            }
-
-        }
-    }
+		}
+	}
 }
